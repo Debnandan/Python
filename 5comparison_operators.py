e21b39ca818904fa1af0f185976dc9d952d300ef
Debnandan <debnandanbera24@gmail.com>
2025-06-21 00:18:28 +0530

    adding programs

---
+++ 5comparison_operators.py
@@ -0,0 +1,43 @@
+# == 
+print("==")
+print(1 == 2)
+print(1.0 == 1)
+print(False == True)
+print(True == 1)
+print(False == 0)
+print('A' == "A")
+print('A' == "a")
+print("------------------------")
+
+# != 
+print("!=")
+print(1 != 2 )
+print(1.0 != 1)
+print(False != True)
+print(True != 1)
+print(False != 0)
+print('A' != "A")
+print('A' != "a")
+print("------------------------")
+
+# <
+print("<")
+print(1<2)
+print("------------------------")
+
+# >
+print(">")
+print(2>1)
+print("------------------------")
+
+# <=
+print("<=")
+print(1<=2)
+print(2<=2)
+print("------------------------")
+
+# >=
+print(">=")
+print(1>=2)
+print(2>=2)
+print("------------------------")
---
+++ 7typecasting.py
@@ -0,0 +1,8 @@
+#int
+print("int")
+print(3.14)
+print(int(3.14))
+print(int(' 10 ')) # during typecasting of string it should be number, and without spaces in between numbers / multiple numbers
+print(int(True))
+print(int(False))
+print("------------------")
