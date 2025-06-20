7b1658b98cdde81ae9a42aa3ce20785f9b9fa5dc
Debnandan <debnandanbera24@gmail.com>
2025-06-20 22:01:39 +0530

    adding programs

---
+++ 2datatypes.py
@@ -0,0 +1,15 @@
+print("int------------------")
+print(1)
+print(100)
+print(type(-100))
+
+print("float-----------------")
+print(1.0)
+print(type(-1.0))
+
+print("string----------------")
+print("hello there ' how are you !")
+
+print("boolean----------------")
+print(True)
+print(type(False))
---
+++ 4arithmetic_operators.py
@@ -0,0 +1,48 @@
+print("addition-------------------")
+print(2+3)
+print(2.5+3.5)
+print(2+3.5)
+print("hello "+"buddy")
+print(True+True) #True=1 , False=0
+
+print("subtraction-------------------")
+print(2-3)
+print(3.5-2.5)
+print(2-1.3)
+print(False-True)
+
+print("multiplication-------------------")
+print(2*3)
+print(3.5*2.5)
+print(2*1.3)
+print(False*True)
+
+print("division-------------------")
+print(5/2)
+print(3.5/2.5)
+print(2/1.3)
+
+print("floor division-------------------") #return only the intezer part quotient
+print(5//2)
+print(3.5//2.5)
+print(2//1.3)
+
+print("modulo-------------------")
+print(5%2)
+print(3.5%2.5)
+print(2%1.3)
+
+print("exponentiation-------------------")
+print(5 ** 2)
+print(3.5 ** 2.5)
+print(2 ** 1.3)
+
+print("abs-------------------") #absolute (print the output +ve)
+print(abs(-43))
+print(abs(-4.5))
+
+print("round-------------------")
+print(round(1.23456789,3))
+
+print("power-------------------")
+print(pow(5,6))
