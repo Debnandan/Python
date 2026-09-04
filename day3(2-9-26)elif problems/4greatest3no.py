22bc6b1b426b771128185ad67249836988e92f43
DEBNANDAN BERA <116198506+Debnandan@users.noreply.github.com>
2026-09-04 22:31:49 +0530

    Add files via upload

---
+++ day3(2-9-26)elif problems/4greatest3no.py
@@ -0,0 +1,10 @@
+#WAP to check greatest of 3 numbers.
+n1=int(input("enter number : "))
+n2=int(input("enter number : "))
+n3=int(input("enter number : "))
+if n1>n2 and n1>n3:
+    print("first number greater")
+elif n2>n3:
+    print("second number greater")
+else:
+    print("third number greater")
